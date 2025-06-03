2146197191a452c566c70cfcd27ff5b149ce2213
Loon04 <anahigonzalezj@gmail.com>
2025-06-03 19:29:07 -0300

    T10 Agregar funcionalidad 'agregar al carrito'

--- Frontend/pages/Inicio/inicio.js
+++ Frontend/pages/Inicio/inicio.js
@@ -1,41 +1,75 @@
-function ejecutarInicio() {
-    const usuario = localStorage.getItem("usuario");
+localStorage.removeItem('carrito'); //POR AHORA
 
+async function ejecutarInicio() {
+    const usuario = localStorage.getItem("usuario");
+    
     if (!usuario) {
         window.location.href = "../../index.html"
     } else {
-        cargarProductos();
+        await cargarDatos()
+        cargarProductos(data);
     }
 }
 
-async function cargarProductos() {
+let carrito = JSON.parse(localStorage.getItem('carrito')) || []; //aca esta el get
+
+async function cargarProductos(data) {
     const productContainer = document.getElementsByClassName("product-container")[0];
-    const request = await fetch("../../db/test-productos.json");
-    const data = await request.json();
-
-
+    const fragment = document.createDocumentFragment();
+    //const request = await fetch("../../db/test-productos.json");
+    //const data = await request.json();
+    
+    
     productContainer.innerHTML = "";
-
-
+    
+    
     data.forEach(element => {
         if (element.activo) {
-            productContainer.innerHTML += `
-            <div class="card m-2" style="width: 18rem;">
+            const productDiv = document.createElement('div');
+            productDiv.className = 'card m-2';
+            productDiv.style.width = '18rem';
+            productDiv.innerHTML += `
             <img src="${element.imagen}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${element.nombre}</h5>
                 <p class="card-text">${element.descripcion}</p>
                 <a href="#" class="btn btn-primary">Agregar al carrito</a>
-            </div>
-        </div>
-            `
+                </div>
+        </div>`
+
+        fragment.appendChild(productDiv);
+        
+        actualizarCarrito(productDiv,'.btn',element)
         }
-
     });
+    productContainer.appendChild(fragment);
+}
 
 
+async function cargarDatos() {
+    try {
+        const request = await fetch("../../db/test-productos.json");
+        data = await request.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
 
+function seleccion(container, selector){
+    const funBoton = container.querySelector(selector)
+    return funBoton
+}
 
+function actualizarCarrito(container, selector, producto){
+    seleccion(container, selector).addEventListener('click',() => {
+            carrito.push(producto)
+            console.log(carrito);
+            save()
+    })
+}
+
+function save(){
+    localStorage.setItem('carrito', JSON.stringify(carrito)); //aca esta el set
 }
 
 ejecutarInicio();
