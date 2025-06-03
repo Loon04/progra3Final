258a4da92d5767569740bf8e2797b996b1303812
lucas <lucas.200061@gmail.com>
2025-06-03 19:37:40 -0300

    T09  Implementar paginación de productos

--- Frontend/pages/Inicio/inicio.js
+++ Frontend/pages/Inicio/inicio.js
@@ -9,17 +9,65 @@ function ejecutarInicio() {
 }
 
 async function cargarProductos() {
-    const productContainer = document.getElementsByClassName("product-container")[0];
+
     const request = await fetch("../../db/test-productos.json");
     const data = await request.json();
+    const pagContainer = document.getElementById("pag-container");
+    const productosRenderizar = 3;
+    console.log(data)
+    const pagData = data.slice(0, productosRenderizar);
+    console.log(pagData);
+    renderizarProductos(pagData);
+
+    pagContainer.innerHTML = ` 
+                    <nav aria-label="Page navigation example" style="width: 17rem;">
+                        <ul class="pagination">
+                            <li class="page-item"><a class="btn page-link" id="prev"">Previous</a></li>
+                            <li class="page-item"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item"><a class="page-link" href="#">4</a></li>
+                            <li class="page-item"><a class="btn page-link" id="next">Next</a></li>
+                        </ul>
+                    </nav>`
+
+    paginator(data, productosRenderizar);
 
 
+}
+function paginator(data, productosRenderizar) {
+    const prev = document.getElementById("prev");
+    const next = document.getElementById("next");
+    let indiceActual = 0;
+
+    function mostrarPagina() {
+        const slicedData = data.slice(indiceActual, indiceActual + productosRenderizar);
+        console.log(slicedData);
+        renderizarProductos(slicedData);
+    }
+
+    next.addEventListener("click", () => {
+        if (indiceActual + productosRenderizar < data.length) {
+            indiceActual += productosRenderizar;
+            mostrarPagina();
+        }
+    });
+
+    prev.addEventListener("click", () => {
+        if (indiceActual - productosRenderizar >= 0) {
+            indiceActual -= productosRenderizar;
+            mostrarPagina();
+        }
+    });
+}
+function renderizarProductos(data) {
+    const productContainer = document.getElementsByClassName("product-container")[0];
     productContainer.innerHTML = "";
 
+    data.forEach((element, index) => {
 
-    data.forEach(element => {
-        if (element.activo) {
-            productContainer.innerHTML += `
+
+        productContainer.innerHTML += `
             <div class="card m-2" style="width: 18rem;">
             <img src="${element.imagen}" class="card-img-top" alt="...">
             <div class="card-body">
@@ -29,13 +77,8 @@ async function cargarProductos() {
             </div>
         </div>
             `
-        }
+
 
     });
-
-
-
-
 }
-
 ejecutarInicio();
