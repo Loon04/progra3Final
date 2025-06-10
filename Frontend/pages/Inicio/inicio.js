617fbac375fd3a4466c1187f0209aabcee593aeb
lucas <lucas.200061@gmail.com>
2025-06-10 10:12:52 -0300

    [add] icons

--- Frontend/pages/Inicio/inicio.js
+++ Frontend/pages/Inicio/inicio.js
@@ -203,16 +203,21 @@ function cambiadorTema() {
         body.classList.add("darkLinks");
         headerContainer.classList.add("darkHeader");
         footer.classList.add("darkFooter");
+        btnTema.innerHTML = `<i class="fa-solid fa-sun fa-lg"></i> Tema`;
     } else {
         //si no se quitan
         body.classList.remove("dark");
         body.classList.remove("darkLinks")
         headerContainer.classList.remove("darkHeader");
         footer.classList.remove("darkFooter");
+        btnTema.innerHTML = `<i class="fa-solid fa-moon fa-lg"></i> Tema`;
     }
     //y el click hace toggle a las clases 
     btnTema.addEventListener("click", () => {
         let darkMode = body.classList.toggle("dark");
+        btnTema.innerHTML = darkMode
+            ? `<i class="fa-solid fa-sun fa-lg"></i> Tema`
+            : `<i class="fa-solid fa-moon fa-lg"></i> Tema`;
         body.classList.toggle("darkLinks");
         headerContainer.classList.toggle("darkHeader");
         footer.classList.toggle("darkFooter");
