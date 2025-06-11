02c31d0e90ca0c49285350d4ba0e9a42d5ae306d
lucas <lucas.200061@gmail.com>
2025-06-11 18:27:34 -0300

    seteo base api

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,23 +1,28 @@
 import express from "express";
-import routerConfig from "./src/routes/index.routes.js";
+import routerConfig from "./routes/index.routes.js";
+import { join, __dirname } from "./utils/utils.js";
 
 const configBasicaAPI = (app) => {
     app.use(express.json()); // pueda recibir jsons
     app.use(express.urlencoded({ extended: true })); // pueda recibir forms 
 }
-const configuracionRouter = (app) => {
+const configuracionRouter = (app) => { // configuracion de las rutas 
     app.use("/api/", routerConfig.rutas_init())
 }
-
-const configuracionMotorPlantillas = (app) => {
+const configuracionStatic = (app) => { // configuracion de archivos estaticos
+    app.use(express.static(join(__dirname, "public")));
+}
+const configuracionMotorPlantillas = (app) => { // configuracion de motor de plantillas 
     app.set("view engine", "ejs");
-    app.set("views", "./views");
+    app.set("views", join(__dirname, "views"));
 }
 
 const init = () => {
     const app = express();
 
     configBasicaAPI(app);
+    configuracionMotorPlantillas(app);
+    configuracionStatic(app);
     configuracionRouter(app);
 
     const PORT = 5000;
---
+++ Backend/src/utils/utils.js
@@ -0,0 +1,8 @@
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = join(dirname(__filename), "..");
+
+export { join, __dirname }
