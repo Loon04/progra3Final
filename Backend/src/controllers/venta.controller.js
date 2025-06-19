4b6a89584e287c65468718a92a06ff4b8764bcc3
lucas <lucas.200061@gmail.com>
2025-06-19 16:24:44 -0300

    [fix] fixes controller

--- Backend/src/controllers/venta.controller.js
+++ Backend/src/controllers/venta.controller.js
@@ -5,6 +5,14 @@ export default {
     const { productos, total, nombreUsuario } = req.body;
     try {
 
+      for (const item of productos) {
+        const producto = await Producto.findByPk(item.id);
+
+        if (producto.stock < item.cantidad) { //solo para el uso de prueba, se tiene que arreglar en el front
+          return res.status(400).send(`Stock insuficiente para ${producto.nombre}`);
+        }
+      }
+
       const nuevaVenta = await Venta.create({ //pq
         nombreUsuario,
         total,
@@ -14,20 +22,18 @@ export default {
       for (const item of productos) {
         const producto = await Producto.findByPk(item.id);
 
-        if (producto.stock < item.cantidad) { //solo para el uso de prueba, se tiene que arreglar en el front
-          return res.status(400).send(`Stock insuficiente para ${producto.nombre}`);
-        }
-
         producto.stock -= item.cantidad;
         await producto.save(); //funcion de sequelize para actualizar (UPDATE producto...)
         //addProducto es un metodo generado por sequelize el cual inserta datos en la tabla intermedia
         //en este caso agarramos la venta y le insertamos el producto en el primer parametro..
         // luego con el objeto through le agregamos los campos extras (cantidad)
         // como estamos dentro del for hacemos este proceso por cada producto que este en el carrito..
+
+
         await nuevaVenta.addProducto(producto, { through: { cantidad: item.cantidad } });
 
       }
-      return res.status(201).send("Exito al crear venta" + nuevaVenta.id);
+      return res.status(201).json({ message: "Exito al crear venta", id_venta: nuevaVenta.id });
     } catch (error) {
       console.error("Error al crear venta:", error);
       return res.status(500).send("Error en la base de datos");
