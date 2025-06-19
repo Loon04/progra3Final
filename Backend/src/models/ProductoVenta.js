966d7808fe801d4aa041fdefe52e2a682bf3ccd2
lucas <lucas.200061@gmail.com>
2025-06-19 15:36:08 -0300

    [add] comentarios explicativos

--- Backend/src/models/ProductoVenta.js
+++ Backend/src/models/ProductoVenta.js
@@ -2,7 +2,10 @@ import { DataTypes } from "sequelize";
 import sequelize from "../database/dbConfig.js";
 
 
-
+//para agregar campos extra en la tabla intermedia
+//tenemos que definir el modelo en nuestra app de node
+//para que sequelize nos permita agregar mas campos en este caso 
+// CANTIDAD ... 
 const ProductoVenta = sequelize.define("productos_ventas", {
     cantidad: {
         type: DataTypes.INTEGER,
--- Backend/src/models/relaciones.js
+++ Backend/src/models/relaciones.js
@@ -2,6 +2,10 @@ import Producto from "./Producto.js";
 import ProductoVenta from "./ProductoVenta.js";
 import Venta from "./Venta.js";
 
+// Aca en el configurador de relaciones creamos la relacion entre modelos
+// para que sequelize nos cree la tabla intermedia producto_ventas
+
+// Entonces generamos a partir del modelo que creamos ProductoVenta la tabla intermedia .... 
 Producto.belongsToMany(Venta, { through: ProductoVenta, foreignKey: 'productoId', otherKey: 'ventaId' });
 Venta.belongsToMany(Producto, { through: ProductoVenta, foreignKey: 'ventaId', otherKey: 'productoId' });
 
