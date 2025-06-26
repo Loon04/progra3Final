588d5c5761c9208b8d60f11c749d1758928aa7a3
lucas <lucas.200061@gmail.com>
2025-06-26 12:39:20 -0300

    fixes

--- Backend/src/middlewares/multerConfig.js
+++ Backend/src/middlewares/multerConfig.js
@@ -1,16 +1,21 @@
 import multer from "multer"
 import path from 'path'
+import fs from "fs"
 import { join, __dirname } from "../utils/utils.js"
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => { //cb -> callback
-    try {
-        const uploadPath = cb(null, join(__dirname, "public", "img", "imgProductos")); //(lo que pasa si da error,lo que devuelve si no hay error) 
-    } catch (error) {
-        console.error(error)
-    }
+        const uploadPath = join(__dirname, "public", "img", "imgProductos"); //(lo que pasa si da error,lo que devuelve si no hay error) 
+        try {
+            if (!fs.existsSync(uploadPath)) {
+                fs.mkdirSync(uploadPath, { recursive: true });
+            }
+            cb(null, uploadPath);
+        } catch (error) {
+            console.error(error)
+        }
     },
-    filename: (req, file, cb) => { 
+    filename: (req, file, cb) => {
         const ext = path.extname(file.originalname); //le doy la extencion de la imagen original
         cb(null, file.fieldname + '-' + req.body.nombre + ext); //el nombre se puede cambiar
     }
